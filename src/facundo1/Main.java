c88f56c7525331769cb29a87460e06056400a53a
facundo <jefferfacundo591@gmail.com>
2024-08-28 17:26:21 +0800

    hirap naman

---
+++ src/facundo1/Main.java
@@ -0,0 +1,11 @@
+
+package facundo1;
+
+
+public class Main {
+    public static void main(String[]args){
+        Product pr = new Product();
+        pr.addProducts(11001, "Sabon", 20.00, 20, 10);
+        pr.viewProducts();
+    }
+}
