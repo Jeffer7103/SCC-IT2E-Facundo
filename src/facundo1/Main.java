d1079c3ebdd3c336a5c81deb2a0256c5e36eebb4
jopiajay23@gmail.com <christopherechavez13@gmail.com>
2024-09-04 17:51:31 +0800

    MAHIRAP

--- src/facundo1/Main.java
+++ src/facundo1/Main.java
@@ -5,7 +5,12 @@ package facundo1;
 public class Main {
     public static void main(String[]args){
         Product pr = new Product();
-        pr.addProducts(11001, "Sabon", 20.00, 20, 10);
-        pr.viewProducts();
+        account ac = new account();
+        Grades grs = new Grades();
+        
+//        pr.addProducts(11001, "Sabon", 20.00, 20, 10);
+//        pr.viewProducts();
+
+        ac.getAccount();
     }
 }
